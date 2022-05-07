393cdda2e2cf897ea974eb638e4193656d416778
springland <fenglinmail@yahoo.com>
2022-05-07 18:50:07 -0400

    update booklist

--- booklist/src/Booklist.js
+++ booklist/src/Booklist.js
@@ -1,40 +1,15 @@
 import React from 'react'
 
+import {books} from './books'
+
+import Book from './Book'
+
 const firstbook = {
     img:'https://m.media-amazon.com/images/I/516z-D3WmCL.jpg',
     title:'The Man Burned by Winter',
     author:'Pete Zacharias'
 }
 
-const books = [
-    {
-        id: 1,
-        img:'https://m.media-amazon.com/images/I/41EJlmFJUDL._SY346_.jpg',
-        title:'Last Summer Boys',
-        author:'Bill Rivers'
-    },
-    {
-        id: 2,
-        img:'https://m.media-amazon.com/images/I/51smsVgcV1L._SY346_.jpg',
-        title:'The Candid Life of Meena Dave',
-        author:'Namrata Patel'
-    },
-    {
-        id : 3,
-        img:'https://m.media-amazon.com/images/I/41dVg1WexcL.jpg',
-        title:'The Peacekeeper',
-        author:'B.L. Blanchard'
-    },
-    {
-        id : 4,
-        img:'https://m.media-amazon.com/images/I/51Sheq9pqwL._SY346_.jpg',
-        title:'The Community: A Memoir',
-        author:'N. Jamiyla Chisholm'
-    }
-
-
-
-];
 
 function Booklist() {
   return (
@@ -87,57 +62,5 @@ function Booklist() {
 }
 
 
-function Book(props)
-{
-    const {img , title} = props
-
-    const clickHanlder = (e) => {
-            console.log(e);
-            console.log(e.target);
-            alert('Hello World');
-    }
-
-    const complexExampleHandler = (author) => {
-        alert(author)
-    }
-    return (
-
-        <article className='book'  onMouseOver={ () => {
-            console.log('mouse over book  ' + title);
-        }}>
-            <Image img={img} />
-            <Title name={title}/>
-            <Author name={props.author}/>
-            <button type='button' onClick={clickHanlder}>reference example</button>
-            <button type='button' onClick={() =>complexExampleHandler(props.author)}>complex example</button>
-            {props.children}
-        </article>
-
-    )
-}
-
-function Image({img})
-{
-    return (
-        <img src={img} ></img>
-    )
-}
-
-const Author = (props) => {
-    return (
-            <h4 style={{color:'#617d98' , fontSize:'0.75rem'}}>{props.name}</h4>
-    )
-};
-
-const Title = (props) => 
-{
-    //const title = 'Title: The Match'
-    return (
-        <div>
-            <h2>{props.name}</h2>
-            <p> {6+6}</p>
-        </div>
-    )
-};
 
 export default Booklist ;
